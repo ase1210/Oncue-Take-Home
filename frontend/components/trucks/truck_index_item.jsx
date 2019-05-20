5f4f98670ea52ce8496d6a3ef7e1246dd57a8a1a
DrewE <andrew.engelstein@gmail.com>
2019-05-20 16:41:36 -0700

    Update TruckIndexItems text to match wireframe

--- frontend/components/trucks/truck_index_item.jsx
+++ frontend/components/trucks/truck_index_item.jsx
@@ -4,9 +4,9 @@ import TruckJobItem from "./truck_job_item";
 const TruckIndexItem = props => (
   <>
     <div className="truck-item">
-      <p>{props.truck.name}</p>
+      <p>{props.truck.name} assignments:</p>
       {props.jobs.length === 0 ? (
-        <p>No jobs booked</p>
+        <p>No assignments</p>
       ) : (
         props.jobs.map(job => <TruckJobItem key={job.id} job={job} />)
       )}
