19cf4aab7764081d5c419d0488e91ad3b82270ee
DrewE <andrew.engelstein@gmail.com>
2019-05-22 18:33:38 -0700

    Cleaned up JobController #create logic

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -4,17 +4,12 @@ class Api::JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new
-    @job.name = params[:job][:name]
-    @job.date = params[:job][:date]
-    @job.start = Time.new(2000, 1, 1, params[:job][:s_hour], params[:job][:s_min]) if params[:job][:s_hour] && params[:job][:s_min]
-    @job.end = Time.new(2000, 1, 1, params[:job][:e_hour], params[:job][:e_min]) if params[:job][:e_hour] && params[:job][:e_min]
+    j_params = params[:job]
+    @job = Job.new(name: j_params[:name], date: j_params[:date])
+    @job.start = Time.new(2000, 1, 1, j_params[:s_hour], j_params[:s_min]) if j_params[:s_hour] && j_params[:s_min]
+    @job.end = Time.new(2000, 1, 1, j_params[:e_hour], j_params[:e_min]) if j_params[:e_hour] && j_params[:e_min]
 
     @job.truck_id = Truck.find_open_truck(@job.date, @job.start, @job.end)
-
-    # if truck
-    #   @job.truck_id = truck
-    # end
 
     if @job.save
       render :show
